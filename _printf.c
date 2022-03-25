b33c824df66ff8d7cf6a8c3b1c8e5e8a6ab1e58d
MaoScorpion21 <mao7021@hotmail.com>
2022-03-25 18:49:52 -0500

    Fix 2 indentation _printf.c

--- _printf.c
+++ _printf.c
@@ -5,6 +5,11 @@
 #include <stdarg.h>
 #include "main.h"
 
+/**
+ * _printf - Start function for printf project
+ * @format: String to print with formatting options
+ * Return: -1 on failure, lenght of printed string otherwise
+ */
 
 
 int _printf(const char *format, ...)
@@ -12,80 +17,78 @@ int _printf(const char *format, ...)
 	int i = 0, num;
 	int len = 0;
 	unsigned int ui;
- 	char letter;
-        char *str;
+	char letter;
+	char *str;
 	char ustr[64];
 
 	char buffer[sizeof(char) * 512];
 	char nstr[64];
 
-        va_list arguments;
+	va_list arguments;
 
-        va_start(arguments, format);
+	va_start(arguments, format);
 
-        while(format[i] != '\0')
-        {
-                if (format[i] != '%')
+	while (format[i] != '\0')
+	{
+	if (format[i] != '%')
+	{
+		buffer[len] = format[i];
+		len++;
+	}
+	if (format[i] == '%')
+	{
+		i++;
+		switch (format[i])
 		{
+		case 'c':
+			letter = va_arg(arguments, int);
+/*write(1, &letra, 1);*/
+			buffer[len] = letter;
+			len++;
+			break;
+		case 'i':
+			num = va_arg(arguments, int);
+			int_str(num, nstr);
+/*write(1, &nstr, 4);*/
+			len = _addstr(buffer, nstr, len);
+			break;
+		case 'd':
+			num = va_arg(arguments, int);
+			int_str(num, nstr);
+/*write(1, &nstr, 4);*/
+			len = _addstr(buffer, nstr, len);
+			break;
+		case 's':
+			str = va_arg(arguments, char *);
+/*print_str(str, buffer, len);*/
+			len = _addstr(buffer, str, len);
+/* write(1, &str[k], 1);*/
+			break;
+		case 'u':
+			ui = va_arg(arguments, unsigned int);
+			uint_str(ui, ustr, 10);
+			len = _addstr(buffer, ustr, len);
+			break;
+		case 'o':
+			ui = va_arg(arguments, unsigned int);
+			uint_str(ui, ustr, 8);
+			len = _addstr(buffer, ustr, len);
+			break;
+		case '%':
+			buffer[len] = '%';
+			len++;
+			break;
+
+		default:
+			--i;
 			buffer[len] = format[i];
 			len++;
 		}
-                if (format[i] == '%')
-                {
-                        i++;
-                        switch (format[i])
-                        {
-                                case 'c':
-                                        letter = va_arg(arguments, int);
-                                        /*write(1, &letra, 1);*/
-					buffer[len] = letter;
-					len++;
-					break;
-                                case 'i':
-					num = va_arg(arguments, int);
-					int_str(num, nstr);
-					/*write(1, &nstr, 4);*/
-					len = _addstr(buffer, nstr, len);
-					break;
-				case 'd':
-					num = va_arg(arguments, int);
-					int_str(num, nstr);
-					/*write(1, &nstr, 4);*/
-					len = _addstr(buffer, nstr, len);
-					break;
-				case 's':
-
-				       	str = va_arg(arguments, char *);
-					/*print_str(str, buffer, len);*/
-					len = _addstr(buffer, str, len);
-                                        /* write(1, &str[k], 1);*/
-                                        break;
-				case 'u':
-					ui = va_arg(arguments, unsigned int);
-					uint_str(ui, ustr, 10);
-					len = _addstr(buffer, ustr, len);
-					break;
-				case 'o':
-					ui = va_arg(arguments, unsigned int);
-					uint_str(ui, ustr, 8);
-					len = _addstr(buffer, ustr, len);
-					break;
-				case '%':
-					buffer[len] = '%';
-					len++;
-					break;
-
-                                default:
-					--i;
-					buffer[len] = format[i];
-					len++;
-                        }
-                }
-                i++;
-        }
-	buffer[len] = '\0';
-	write(1, &buffer, len);
-	va_end(arguments);
-
-	return (len);
+	}
+	i++;
+}
+buffer[len] = '\0';
+write(1, &buffer, len);
+va_end(arguments);
+return (len);
 }
