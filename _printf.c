98d2dc19fc1d380617be091d0c9cd3cf27df6019
soft-insight <j0108rodriguez@gmail.com>
2022-03-25 17:22:44 -0500

    added unknowm case

--- _printf.c
+++ _printf.c
@@ -5,103 +5,11 @@
 #include <stdarg.h>
 #include "main.h"
 
-/*
-int _addstr(char *buffer, char s[],int n)
-{
-	int x = n;
-	int y = 0;
-
-	while(s[y] != '\0')
-	{	buffer[x] = s[y];
-		x++;
-		y++;
-	}
-	return(n + y);
-}
-*/
-
-/*
-int _strlen(char *s)
-{
-	int i;
-
-	i = 0;
-
-	while (s[i] != '\0')
-	{
-		i++;
-	}
-
-	return (i);
-}
-*/
-
-/**
- *
- *
- */
-
-/*
-int print_str(char str[], char *buffer, int len)
-{
-	int leng, k;
-	leng = _strlen(str);
-
-	for (k = 0; k < leng; k++)
-	{
-		buffer[len] = str[k];
-		len++;
-	}
-	return (len);
-
-//	printf("the length of the string %d\n", len);
-}
-*/
-
- /* reverse:  reverse string s in place */
-
-
-/* void reverse(char str[])
-{
-    int i, len, temp;
-    len = _strlen(str); */ /* use strlen() to get the length of str string*/
-
-    /* use for loop to iterate the string*/
-/*    for (i = 0; i < len/2; i++)
-    {  */
-        /* temp variable use to temporary hold the string*/
-/*        temp = str[i];
-        str[i] = str[len - i - 1];
-        str[len - i - 1] = temp;
-    }
- }
-*/
-
- /* int_str:  convert n to characters in s */
-/*
- void int_str(int n, char s[])
- {
-	 int i, sign;
-	 //num = va_arg(arguments, int);
-
-     if ((sign = n) < 0)
-         n = -n;          // make n positive
-     i = 0;
-     do {       // generate digits in reverse order
-         s[i++] = n % 10 + '0';   // get next digit
-     } while ((n /= 10) > 0);     // delete it
-     if (sign < 0)
-         s[i++] = '-';
-     s[i] = '\0';
-     reverse(s);
-
- }
- */
 
 
 int _printf(const char *format, ...)
 {
-        int i = 0, num;
+	int i = 0, num;
 	int len = 0;
 	unsigned int ui;
  	char letter;
@@ -119,7 +27,7 @@ int _printf(const char *format, ...)
         while(format[i] != '\0')
         {
                 if (format[i] != '%')
-		{       /* write(1, &format[i], 1);*/
+		{
 			buffer[len] = format[i];
 			len++;
 		}
@@ -164,7 +72,9 @@ int _printf(const char *format, ...)
 					break;
 				
                                 default:
-                                        printf("Error, not valid specifier");
+					--i;
+					buffer[len] = format[i];
+					len++;
                         }
                 }
                 i++;
