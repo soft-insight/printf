cddd46058caa01d46e85bc8975cb67a466aa26ba
jesus <jmmacias@uninorte.edu.co>
2022-03-28 22:24:43 -0500

    commit for fork

--- case_functions.c
+++ case_functions.c
@@ -5,182 +5,177 @@
 #include <stdarg.h>
 #include "main.h"
 
-/*_addstr - add a string to a buffer
- *@buffer : pointer to a buffer
- *@s : pointer to string
- *@n : size of the buffer
- *Return : new size of buffer
+/**
+ * _addstr - add a string to a buffer
+ * @buffer : pointer to a buffer
+ * @s : pointer to string
+ * @n : size of the buffer
+ * Return : new size of buffer
  */
-int _addstr(char buffer[], char s[],int n)
+int _addstr(char buffer[], char s[], int n)
 {
-        int x = n;
-        int y = 0;
+	int x = n;
+	int y = 0;
 
-        while(s[y] != '\0')
-        {       buffer[x] = s[y];
-                x++;
-                y++;
-        }
-        return(n + y);
+	while (s[y] != '\0')
+	{
+		buffer[x] = s[y];
+		++;
+		y++;
+	}
+	return (n + y);
 }
 
-/*_strlen : find the size of a string
- *@s : pointer to string
- *Return : length of string
+/**
+ * _strlen : find the size of a string
+ * @s : pointer to string
+ * Return : length of string
  */
 int _strlen(char *s)
 {
-        int i;
+	int i;
 
-        i = 0;
+	i = 0;
 
-        while (s[i] != '\0')
-        {
-                i++;
-        }
+	while (s[i] != '\0')
+	{
+		i++;
+	}
 
-        return (i);
+	return (i);
 }
 
-/*
+/**
+ * print_str
  *
  *
  */
 
 int print_str(char *str, char buffer[], int len)
 {
-        int leng, k;
-        leng = _strlen(str);
+	int leng, k;
 
-        for (k = 0; k < leng; k++)
-        {
-                buffer[len] = str[k];
-                len++;
-        }
-        return (len);
+	leng = _strlen(str);
+
+	for (k = 0; k < leng; k++)
+	{
+		buffer[len] = str[k];
+		len++;
+	}
+	return (len);
 }
 
 
  /* reverse:  reverse string s in place */
- void reverse(char str[])
+void reverse(char str[])
 {
-    int i, j, temp;
-    j = _strlen(str); /* use strlen() to get the length of str string*/
+	int i, j, temp;
 
-    /* use for loop to iterate the string*/
-    for (i = 0; i < j/2; i++)
-    {
-        /* temp variable use to temporary hold the string*/
-        temp = str[i];
-        str[i] = str[j - i - 1];
-        str[j - i - 1] = temp;
-    }
- }
+	j = _strlen(str); /* use strlen() to get the length of str string */
 
- /*int_str - convert a integer to string
-  *@n : integer
-  *@s : pointer to string
-  */
- void int_str(long int n, char s[])
- {
-         int i, sign;
-         /* num = va_arg(arguments, int);*/
+/* use for loop to iterate the string */
+	for (i = 0; i < j / 2; i++)
+	{
+/* temp variable use to temporary hold the string*/
+		temp = str[i];
+		str[i] = str[j - i - 1];
+		str[j - i - 1] = temp;
+	}
+}
 
-     if ((sign = n) < 0)
-         n = -n;          /* make n positive */
-     i = 0;
-     do {       /* generate digits in reverse order */
-         s[i++] = n % 10 + '0';   /* get next digit */
-     } while ((n /= 10) > 0);     /* delete it */
-     if (sign < 0)
-         s[i++] = '-';
-     s[i] = '\0';
-     reverse(s);
+/**
+ * int_str - convert a integer to string
+ * @n : integer
+ * @s : pointer to string
+ */
+void int_str(long int n, char s[])
+{
+	int i, sign;
+/* num = va_arg(arguments, int); */
 
- }
-
-
- void uint_str(unsigned int n, char s[],unsigned int base)
- {	
-         int i;
-         /* num = va_arg(arguments, int);*/
-	
+	if ((sign = n) < 0)
+		n = -n;          /* make n positive */
 	i = 0;
-          
-     	do {       /* generate digits in reverse order */
-		
-		if(n % base < 10)
+	do {       /* generate digits in reverse order */
+		s[i++] = n % 10 + '0';   /* get next digit */
+	} while ((n /= 10) > 0);     /* delete it */
+	if (sign < 0)
+		s[i++] = '-';
+	s[i] = '\0';
+	reverse(s);
+
+}
+
+
+void uint_str(unsigned int n, char s[], unsigned int base)
+{
+	int i;
+/* num = va_arg(arguments, int); */
+
+	i = 0;
+
+	do {       /* generate digits in reverse order */
+
+		if (n % base < 10)
 		{
-                        s[i++] = n % base + '0';   /* get next digit */
-		}else
+			s[i++] = n % base + '0';   /* get next digit */
+		} else
 		{
 			s[i++] = n % base - 10 + 'a';   /* get next digit */
 		}
-		
-     	} while ((n /= base) > 0);     /* delete it */
-	
-	
-		
-	
-	
-     s[i] = '\0';
-     reverse(s);
-	
- }
 
- void uint_str2(long int n, char s[],unsigned int base)
- {
-         int i;
-         /* num = va_arg(arguments, int);*/
+	} while ((n /= base) > 0);     /* delete it */
+	s[i] = '\0';
+	reverse(s);
 
-        i = 0;
+}
 
-        do {       /* generate digits in reverse order */
+void uint_str2(long int n, char s[], unsigned int base)
+{
+	int i;
+/* num = va_arg(arguments, int); */
 
-                if(n % base < 10)
-                {
-                        s[i++] = n % base + '0';   /* get next digit */
-                }else
-                {
-                        s[i++] = n % base - 10 + 'a';   /* get next digit */
-                }
+	i = 0;
 
-        } while ((n /= base) > 0);     /* delete it */
+	do {       /* generate digits in reverse order */
 
+		if (n % base < 10)
+		{
+			s[i++] = n % base + '0';   /* get next digit */
+		}
+		else
+		{
+			s[i++] = n % base - 10 + 'a';   /* get next digit */
+		}
 
+	} while ((n /= base) > 0);     /* delete it */
 
+	s[i] = '\0';
+	reverse(s);
 
+}
 
-     s[i] = '\0';
-     reverse(s);
+void uint_str3(int n, char s[], unsigned int base)
+{
+	int i;
+/* num = va_arg(arguments, int); */
 
- }
+	i = 0;
 
- void uint_str3(int n, char s[],unsigned int base)
- {
-         int i;
-         /* num = va_arg(arguments, int);*/
+	do {       /* generate digits in reverse order */
 
-        i = 0;
+		if (n % base < 10)
+		{
+			s[i++] = n % base + '0';   /* get next digit */
+		}
 
-        do {       /* generate digits in reverse order */
+		else
+		{
+			s[i++] = n % base - 10 + 'A';   /* get next digit */
+		}
 
-                if(n % base < 10)
-                {
-                        s[i++] = n % base + '0';   /* get next digit */
-                }else
-                {
-                        s[i++] = n % base - 10 + 'A';   /* get next digit */
-                }
+	} while ((n /= base) > 0);     /* delete it */
+	s[i] = '\0';
+	reverse(s);
 
-        } while ((n /= base) > 0);     /* delete it */
-
-
-
-
-
-     s[i] = '\0';
-     reverse(s);
-
- }
-
+}
